17c785fcfd2af8e4b4e93690f9beaf702102f068
Anna Chuapetcharasopon <143050387+ahgeak@users.noreply.github.com>
2023-10-21 14:05:10 -1000

    Used day.js to add the current date to the webpage, started working on adding time rows

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,49 @@
+// day.js is used to get the current date and update the #currentDay p element
+var currentDay = dayjs().format("dddd, MMMM D");
+$("#currentDay").text(currentDay);
+
+var hourRowCreate = document.getElementById("hour-");
+var hoursInWorkDay = 2;
+
+var containerFuild = document.getElementsByClassName("container-fluid");
+
+// Elements to create in the HTML file for each hour block: 
+/* 
+<div id="hour-" class="row time-block">
+<div class="col-2 col-md-1 hour text-center py-3">Testing</div>
+<textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+<button class="btn saveBtn col-2 col-md-1" aria-label="save">
+  <i class="fas fa-save" aria-hidden="true"></i>
+</button>
+</div> */
+
+var body = document.body;
+var divParentElement = document.createElement("div");
+var divChildElement = document.createElement("div");
+var textAreaElement = document.createElement("textarea");
+var buttonElement = document.createElement("button");
+
+divParentElement.textContent = "Testing Div Parent";
+divChildElement.textContent = "This is the child element";
+textAreaElement.textContent = "this is the text area";
+
+containerFuild.appendChild(divParentElement);
+containerFuild.appendChild(divChildElement);
+containerFuild.appendChild(textAreaElement);
+
+for (var i =  0; i < hoursInWorkDay; i++){
+  hourRowCreate.id = "hour-1";
+  
+  console.log(hourRowCreate);
+}
+
+function createNewHour () {
+  //change id to include current iteration number (i)
+  // document.getElementById('one').id = 'two';
+  document.hourRowCreate.id = "hour-" + i;
+  console.log(hourRowCreate);
+}
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
